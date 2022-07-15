93226a8fc47100c451327ccc5f72b939ae78e782
jaweria961 <javeriahanif65@gmail.com>
2022-07-15 12:49:42 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is difference between cookies and session?',
+    info:
+      'Cookies are only stored on the client-side machine, while sessions get stored on the client as well as a server.',
+  },
+  {
+    id: 2,
+    title: 'What is a differncce between local storage vs session storage?',
+    info:
+      'sessionStorage is similar to localStorage ; the difference is that while data in localStorage doesnt expire, data in sessionStorage is cleared when the page session ends.',
+  },
+  {
+    id: 3,
+    title: 'What is stopPropagation() Event Method',
+    info:
+      'The stopPropagation() method prevents propagation of the same event from being called.Propagation means bubbling up to parent elements or capturing down to child elements.',
+  },
+ 
+]
+export default questions
